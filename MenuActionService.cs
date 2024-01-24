d992de6a68e775daf2514e5b54363b36ab578463
Ted K <ted.k07@gmail.com>
2024-01-24 22:22:15 +0100

    Add project files.

---
+++ MenuActionService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyCoinsApp
+{
+    public class MenuActionService
+    {
+        private List<MenuAction> menuActions = new List<MenuAction>();
+        //do wyświetlenia powyższej listy potrzeba poniższych metod
+        public void AddNewAction(int id, string name, string menuName)
+        {
+            MenuAction menuAction = new MenuAction() { Id = id, Name = name, MenuName = menuName };
+            menuActions.Add(menuAction);
+        }
+        public List<MenuAction> GetMenuActionsByMenuName(string menuName)
+        {
+            List<MenuAction> result = new List<MenuAction>();
+            //pętla foreach przeliteruje po wszystkich elementach
+            foreach (var menuAction in menuActions)
+            {
+                if (menuAction.MenuName == menuName)
+                {
+                    result.Add(menuAction);
+                }
+            }
+            return result;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿namespace MyCoinsApp
+{
+    internal class Program
+    {
+
+        public const string FILE_NAME = @"C:\MyCoinsApp\Importfile.xlsx";
+        private static object menuList;
+
+        static void Main(string[] args)
+        {
+            //nowy obiekt serwisu
+            MenuActionService actionService = new MenuActionService();
+            actionService = Initialize(actionService);
+
+
+            Console.WriteLine("Welcome to MyCoinsApp !");
+            Console.WriteLine("Please let me know whate you want to do:");
+            var mainMenu = actionService.GetMenuActionsByMenuName("Main");
+
+            for (int i = 0; i < mainMenu.Count; i++)
+            {
+                Console.WriteLine($"{mainMenu[i].Id}. {mainMenu[i].Name}");
+            }
+
+            var operation = Console.ReadKey();
+            //odczytuje tylko 1 znak wprowadzony na klawiaturze
+            switch (operation.KeyChar)
+            {
+
+            }
+
+        }
+        private static MenuActionService Initialize(MenuActionService actionService)
+        {
+            actionService.AddNewAction(1, "Add item", "Main");
+            actionService.AddNewAction(2, "Remove item", "Main");
+            actionService.AddNewAction(3, "Show details", "Main");
+            actionService.AddNewAction(4, "List of Itemss", "Main");
+            actionService.AddNewAction(5, "Test test of Itemss", "Main");
+            return actionService;
+        }
+
+    }
+}
+}
